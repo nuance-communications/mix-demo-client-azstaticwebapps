c61ec9404bceca0f706192ea87474ac343bdd84d
Nirvana Tikku <nirvana.tikku@nuance.com>
2022-06-02 21:15:17 -0400

    Bump to 1.7.0

--- app/src/components/shared.js
+++ app/src/components/shared.js
@@ -11,7 +11,7 @@ import { navigate } from "gatsby"
 import axios from "axios"
 
 export const ROOT_URL = process.env.NODE_ENV === 'production' ? '' : 'https://localhost:7071'
-export const VERSION = '1.4.0'
+export const VERSION = '1.7.0'
 export const CLIENT_DATA = {
     "version": VERSION,
     "client": "Nuance Mix Demo Client",
@@ -583,6 +583,9 @@ export class BaseClass extends React.Component {
     } else if (response) {
       if(response.payload){
         const rawEvents = this.state.rawEvents.concat(response.payload)
+                            // .filter((obj, index, arr) => {
+                            //   return arr.map(mapObj => mapObj.seqid).indexOf(obj.seqid) === index
+                            // })
         toUpdate.rawEvents = rawEvents
       }
     }
