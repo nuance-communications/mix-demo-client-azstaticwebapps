b897d3cc52a1d8e0c7e09156538109f68c0ee364
Nirvana Tikku <nirvana.tikku@nuance.com>
2022-06-02 22:17:58 -0400

    Fix build issue

--- app/src/lib/visualizer.js
+++ app/src/lib/visualizer.js
@@ -12,17 +12,19 @@ import React from "react"
 const BASE_COLOR = 'rgb(33,33,33)'
 const FACTOR = 13
 
-CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-  if (w < 2 * r) r = w / 2;
-  if (h < 2 * r) r = h / 2;
-  this.beginPath();
-  this.moveTo(x+r, y);
-  this.arcTo(x+w, y,   x+w, y+h, r);
-  this.arcTo(x+w, y+h, x,   y+h, r);
-  this.arcTo(x,   y+h, x,   y,   r);
-  this.arcTo(x,   y,   x+w, y,   r);
-  this.closePath();
-  return this;
+if(typeof window !== 'undefined'){
+  CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
+    if (w < 2 * r) r = w / 2;
+    if (h < 2 * r) r = h / 2;
+    this.beginPath();
+    this.moveTo(x+r, y);
+    this.arcTo(x+w, y,   x+w, y+h, r);
+    this.arcTo(x+w, y+h, x,   y+h, r);
+    this.arcTo(x,   y+h, x,   y,   r);
+    this.arcTo(x,   y,   x+w, y,   r);
+    this.closePath();
+    return this;
+  }
 }
 
 export class AudioVisualizer extends React.Component {
